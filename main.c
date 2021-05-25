0302db16dd8528ead3cef788b4836dd4c76ceab0
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-05-25 09:10:45 -0300

    Atualizacao 2505

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+/*Seu programa deve ler vários conjuntos de teste. 
+A primeira linha de um conjunto de teste contém um inteiro não negativo, N, que indica o número de processos a serem lidos e colocados na fila de prontos (o valor N = 0 indica o final da entrada).
+
+Seguem-se N linhas, cada uma contendo um par de números inteiros não negativos X e Y que representam o tempo de ingresso do processo (X) e o tempo de duração do processo (Y). Os processos são numerados sequencialmente a partir de 1, na ordem em que aparecem na entrada.*/
 
 int main()
 {
-    int n = 0;        /*n Número de processos a ser lidos // OBS: Inteiro não negativo  */
-    int x = 0, y = 0; /* x = Tempo de ingresso do processo // y = Tempo de duração do processo // */
+    setlocale(LC_ALL, "Portuguese");
 
-    printf("Por favor, insira o numero de processos a ser lido \n");
+    int n = 0; /*n Número de processos a ser lidos // OBS: Inteiro não negativo  */
+    printf("Insira o numero de processos 'N'\n");
     scanf("%d", &n);
-    
-    printf("%i\n",n);
-    system("pause");
+
+    int x[n], y[n]; /* x = Tempo de ingresso do processo // y = Tempo de duração do processo // */
+
+    int i; // Não declarado dentro do for por imcopatibilidade do copilador usado no VSCODE.
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Insira o tempo de ingresso do processo 'X' [%d]\n", i + 1);
+
+        printf("Insira o tempo de duracao do processo 'Y' [%d]\n", i+1);
+    }
+
+    printf("SUCESSO\n", n);
+    //system("pause");
     return 0;
 }
