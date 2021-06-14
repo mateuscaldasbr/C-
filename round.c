b9598f833c26cf45db11331c3379ae38f2e9deee
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-14 18:12:41 -0300

    Round Beta 02

--- round.c
+++ round.c
@@ -1,70 +1,71 @@
 /*Paulo Mateus de Lima Caldas && Sérgio*/
 /*ALGORITMO DE ROUND ROBIN*/
 
-#include <stdio.h>
-#include <conio.h>
-
-int main(){
-	int n, i, qt,
-	contador = 0,
-	temp,
-	sq = 0,
-	bt[10],
-	wt[10],
-	tat[10],
-	rem_bt[10];;
-	
-	float awt = 0, atat = 0;
-	
-	printf("Insira o número de processos a serem lidos e colocados na fila de prontos:\n ");
-	scanf ("%i", &n);
-	
-	for(i=0; i<n; i++){
-		printf("Tempo de Execucao do processo: ");
-		scanf ("%i", &bt[i]);
-		rem_bt[i] = bt[i];
-	}
-	
-	printf("Insira o valor de Quantum: ");
-	scanf ("%i", &qt);
-	
-	while(1){
-		for(i=0; contador=0; i++){
-			temp = qt;
-			if(rem_bt[i] == 0){
-				contador++;
-				continue;
-			}
-			if(rem_bt[i] > qt){
-				rem_bt[i] = rem_bt[i] - qt;
-			}else{
-				if(rem_bt[i] >= 0){
-					temp = rem_bt[i];
-					rem_bt[i] = 0;
-				}
-				
-				sq = sq + temp;
-				tat[i] = sq;
-			}
-				
-		}
-		if(n==contador)
-			break;
-		
-	}
-	printf("\nProcessos\tTempo de Execução\tTurnaround\tTempo_Espera\n");
-	
-	for(i=0; i<n; i++){
-		wt[i] = tat[i] - bt[i];
-		awt = awt + wt[i];
-		atat = atat + tat[i];
-		
-		printf("\n%i\t%i\t\t%i\t\t%i", i+1, bt[i], tat[i], wt[i]);
-	}
-	
-	awt = awt/n;
-	atat = atat/n;
-	
-	printf("Media de Tempo de Espera: %f", awt);
-	printf("Media Turnaround: %f", atat);	
-}
+    #include<stdio.h>  
+    #include<conio.h>  
+      
+    void main()  
+    {  
+        // initlialize the variable name  
+        int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10];  
+        float avg_wt, avg_tat;  
+        printf(" Total number of process in the system: ");  
+        scanf("%d", &NOP);  
+        y = NOP; // Assign the number of process to variable y  
+      
+    // Use for loop to enter the details of the process like Arrival time and the Burst Time  
+    for(i=0; i<NOP; i++)  
+    {  
+    printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
+    printf(" Arrival time is: \t");  // Accept arrival time  
+    scanf("%d", &at[i]);  
+    printf(" \nBurst time is: \t"); // Accept the Burst time  
+    scanf("%d", &bt[i]);  
+    temp[i] = bt[i]; // store the burst time in temp array  
+    }  
+    // Accept the Time qunat  
+    printf("Enter the Time Quantum for the process: \t");  
+    scanf("%d", &quant);  
+    // Display the process No, burst time, Turn Around Time and the waiting time  
+    printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");  
+    for(sum=0, i = 0; y!=0; )  
+    {  
+    if(temp[i] <= quant && temp[i] > 0) // define the conditions   
+    {  
+        sum = sum + temp[i];  
+        temp[i] = 0;  
+        count=1;  
+        }     
+        else if(temp[i] > 0)  
+        {  
+            temp[i] = temp[i] - quant;  
+            sum = sum + quant;    
+        }  
+        if(temp[i]==0 && count==1)  
+        {  
+            y--; //decrement the process no.  
+            printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
+            wt = wt+sum-at[i]-bt[i];  
+            tat = tat+sum-at[i];  
+            count =0;     
+        }  
+        if(i==NOP-1)  
+        {  
+            i=0;  
+        }  
+        else if(at[i+1]<=sum)  
+        {  
+            i++;  
+        }  
+        else  
+        {  
+            i=0;  
+        }  
+    }  
+    // represents the average waiting time and Turn Around time  
+    avg_wt = wt * 1.0/NOP;  
+    avg_tat = tat * 1.0/NOP;  
+    printf("\n Average Turn Around Time: \t%f", avg_wt);  
+    printf("\n Average Waiting Time: \t%f", avg_tat);  
+    getch();  
+    }  
