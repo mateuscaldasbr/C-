c4e6d646a56e57214441820d97073349bdaa9216
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-14 18:59:46 -0300

    round v1

--- round.c
+++ round.c
@@ -6,7 +6,7 @@
       
     void main()  
     {    
-        int i, n, sum=0,count=0, y, m, wt=0, tat=0, at[10], bt[10], temp[10];  
+        int i, n, sum=0,count=0, y, m, wt=0, tat=0, vetX[10], vetY[10], temp[10];  
         float tempMedioEspera, tempMedioExecucao;  
         printf("O valor do quantum a ser utilizado no escalonamento dos processos\n");  
     scanf("%d", &m); 
@@ -17,10 +17,10 @@
     for(i=0; i<n; i++)  
     {  
     printf("Tempo de ingresso do processo: %i \n",i+1);  
-    scanf("%d", &at[i]);  
+    scanf("%d", &vetX[i]);  /*Vetor 'X'*/
     printf("Tempo de duracao do processo \n");
-    scanf("%d", &bt[i]);  
-    temp[i] = bt[i]; /*Guardar a duracao do processo em um array*/
+    scanf("%d", &vetY[i]);  /*Vetor 'Y'*/
+    temp[i] = vetY[i]; /*Guardar a duracao do processo em um array*/
     }   
     printf("\n Processo \t\t Duracao Processo \t\t TAT \t\t Tempo de espera ");  
     for(sum=0, i = 0; y!=0; )  
@@ -39,16 +39,16 @@
         if(temp[i]==0 && count==1)  
         {  
             y--;  
-            printf("\nProcesso Numero[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
-            wt = wt+sum-at[i]-bt[i];  
-            tat = tat+sum-at[i];  
+            printf("\nProcesso Numero[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, vetY[i], sum-vetX[i], sum-vetX[i]-vetY[i]);  
+            wt = wt+sum-vetX[i]-vetY[i];  
+            tat = tat+sum-vetX[i];  
             count =0;     
         }  
         if(i==n-1)  
         {  
             i=0;  
         }  
-        else if(at[i+1]<=sum)  
+        else if(vetX[i+1]<=sum)  
         {  
             i++;  
         }  
